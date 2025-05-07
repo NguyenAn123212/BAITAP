833eb67203b7e893002dca6324c09c3415ea131d
NguyenAn123212 <nguyenan707mc@gmail.com>
2025-05-07 10:44:13 +0700

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -7,57 +7,48 @@
 #include <string>
 #include <vector> 
 
-// Enum để xác định hành động người dùng chọn từ menu chính
 enum class MenuAction {
     NONE,
     START_GAME,
-    OPEN_OPTIONS, // << THÊM HÀNH ĐỘNG MỞ OPTIONS
+    OPEN_OPTIONS, 
     EXIT_GAME
 };
 
 class Menu {
 public:
-    // Constructor: Nhận renderer, font, kích thước màn hình và nhạc nền
     Menu(SDL_Renderer* renderer, TTF_Font* font, int screenWidth, int screenHeight, Mix_Music* menuMusic);
     ~Menu();
 
-    // Khởi tạo menu, tải hình nền và tạo các nút
     bool initialize(const std::string& backgroundPath);
 
-    // Xử lý sự kiện cho menu (ví dụ: click chuột)
     MenuAction handleEvent(SDL_Event& e);
 
-    // Vẽ menu lên màn hình
     void render();
 
-    // Phát nhạc nền cho menu
     void playMusic();
 
 private:
     SDL_Renderer* mRenderer;
-    TTF_Font* mFont;         // Font được truyền từ lớp Game
+    TTF_Font* mFont;       
     int mScreenWidth;
     int mScreenHeight;
 
-    SDL_Texture* mMenuBackgroundTexture; // Texture cho hình nền của menu
+    SDL_Texture* mMenuBackgroundTexture;
 
-    // Các nút của menu chính
     SDL_Rect mStartButtonRect;
     SDL_Texture* mStartButtonTexture;
 
-    SDL_Rect mOptionsButtonRect;      // << NÚT OPTIONS MỚI
-    SDL_Texture* mOptionsButtonTexture; // << TEXTURE CHO NÚT OPTIONS
+    SDL_Rect mOptionsButtonRect;     
+    SDL_Texture* mOptionsButtonTexture;
 
     SDL_Rect mExitButtonRect;
     SDL_Texture* mExitButtonTexture;
 
-    SDL_Color mButtonTextColor;       // Màu chữ cho các nút
+    SDL_Color mButtonTextColor;      
 
-    Mix_Music* mMenuMusic; // Nhạc nền được truyền từ lớp Game
+    Mix_Music* mMenuMusic;
 
-    // Hàm tiện ích riêng để tạo texture từ text
     SDL_Texture* createTextTexture(const std::string& text, SDL_Color color);
-    // Hàm tiện ích riêng để tải texture hình ảnh
     SDL_Texture* loadTexture(const std::string& path);
 };
 
