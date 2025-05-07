93f48d79851f2887cbb52b4139a495571bfc4ef4
NguyenAn123212 <nguyenan707mc@gmail.com>
2025-05-07 08:15:16 +0700

    Update Enemies.h

--- Enemies.h
+++ Enemies.h
@@ -4,8 +4,7 @@
 #include <SDL.h>
 #include "map.h" // Đảm bảo Map được include nếu Enemy tương tác trực tiếp với nó
 
-// Forward declaration cho Map nếu chỉ dùng con trỏ/tham chiếu và tránh include vòng tròn
-// class Map; 
+
 
 enum Direction {
     UP,
@@ -17,19 +16,17 @@ enum Direction {
 class Enemy {
 public:
     Enemy(SDL_Renderer* renderer, SDL_Texture* texture, int x, int y);
-    ~Enemy() = default; // Sử dụng destructor mặc định là ổn
+    ~Enemy() = default; 
 
     void update(float deltaTime, Map* map);
     void render();
     bool findSafePosition(Map* map);
 
-    // Getters
     int getX() const { return mX; }
     int getY() const { return mY; }
     int getWidth() const { return mWidth; }
     int getHeight() const { return mHeight; }
 
-    // Setters
     void setPosition(int x, int y) { mX = x; mY = y; }
     void setSize(int width, int height); // <<< THÊM DÒNG NÀY
     void changeDirection();
@@ -46,8 +43,7 @@ private:
     float mDirectionChangeTimer;
     float mDirectionChangeCooldown;
 
-    // Phương thức này có thể hữu ích cho logic di chuyển thông minh hơn
     bool canMoveInDirection(Direction dir, Map* map);
 };
 
-#endif // ENEMIES_H
+#endif 
